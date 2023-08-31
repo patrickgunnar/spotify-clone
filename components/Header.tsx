067abdb7998c5f1f5598a77c5f9bb5410515c06a
Patrick Gunnar <112112811+patrickgunnar@users.noreply.github.com>
2023-08-30 22:37:08 -0300

    feat: added search and home handlers on mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -76,14 +76,14 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
                 <div className="flex md:hidden gap-x-2 items-center">
                     <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
-                        type="button" title="Home"
+                        type="button" title="Home" onClick={() => router.push('/')} 
                     >
                         <HiHome className="text-black"
                             size={20}
                         />
                     </button>
                     <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
-                        type="button" title="Search"
+                        type="button" title="Search" onClick={() => router.push('/search')} 
                     >
                         <BiSearch className="text-black"
                             size={20}
